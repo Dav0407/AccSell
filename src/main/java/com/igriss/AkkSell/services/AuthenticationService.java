8453e7f68df47e68337669ac5b3aa6e693a869ef
Davronbek Normamatov <davronnormamatov004@gmail.com>
2024-07-13 23:11:35 +0500

    version 1.0.6 - Code optimization

--- src/main/java/com/igriss/AkkSell/services/AuthenticationService.java
+++ src/main/java/com/igriss/AkkSell/services/AuthenticationService.java
@@ -15,10 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
