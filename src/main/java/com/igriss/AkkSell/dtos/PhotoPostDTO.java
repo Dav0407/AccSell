6594fbead2d1017ae2fffa3d8502bdcdc9197665
Davronbek Normamatov <davronnormamatov004@gmail.com>
2025-01-01 19:37:03 +0500

    build and publish

--- src/main/java/com/igriss/AkkSell/dtos/PhotoPostDTO.java
+++ src/main/java/com/igriss/AkkSell/dtos/PhotoPostDTO.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class PhotoPostDTO {
+
     private List<MultipartFile> photoFiles;
     private String video;
     private String title;
